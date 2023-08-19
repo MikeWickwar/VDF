6ab68a1a0be378a067c358d44a6a15855b249ca8
MikeWickwar <mike@Mikes-MacBook-Pro.local>
2023-08-19 17:33:34 -0600

    build

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ const nodeModulesDir = path.resolve(__dirname, 'node_modules');
 
 //Dont forget  NPM RUN BUILD cmd
 const isDev = process.env.NODE_ENV !== 'production';
-const baseUrl = isDev ? '/' : './';
+// const baseUrl = isDev ? '/' : './';
 const baseUrl = './';
 
 
